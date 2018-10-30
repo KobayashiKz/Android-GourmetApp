94b319437f43b130aec6d8445e92c5b1d5bc2c18
KobayashiKz <anuuihsa9711212@gmail.com>
2018-10-31 02:30:02 +0900

    [recommend]Toolbarの設置

--- app/src/main/java/com/kk/todomvvmapp/gurumeapp/recommend/RecommendActivity.kt
+++ app/src/main/java/com/kk/todomvvmapp/gurumeapp/recommend/RecommendActivity.kt
@@ -1,7 +1,9 @@
 package com.kk.todomvvmapp.gurumeapp.recommend
 
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.kk.todomvvmapp.gurumeapp.R
 
 class RecommendActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class RecommendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recommend)
+
+        // Toolbarの設定
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar? = supportActionBar
+        // ハンバーガーのメニュー画像を設置
+        actionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
