c245462d89d6084afab1e1c96deb99572071bcdd
KobayashiKz <anuuihsa9711212@gmail.com>
2018-11-09 21:21:56 +0900

    [select] 画像選択部分の作成
    ・ドロワーナビゲーション/ツールバーの追加
    ・認証開始ボタンの設置

--- app/src/main/java/com/kk/gourmetapp/select/SelectActivity.kt
+++ app/src/main/java/com/kk/gourmetapp/select/SelectActivity.kt
@@ -1,17 +1,40 @@
 package com.kk.gourmetapp.select
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import com.kk.gourmetapp.R
+import com.kk.gourmetapp.setting.SettingActivity
 import com.kk.gourmetapp.util.ActivityUtil
 
 class SelectActivity : AppCompatActivity() {
 
-    var mSelectPresenter: SelectPresenter? = null
+    private var mDrawerLayout: DrawerLayout? = null
+
+    private var mSelectPresenter: SelectPresenter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select)
+
+        // Toolbarの設定
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar? = supportActionBar
+        // ハンバーガーのメニュー画像を設置
+        actionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // ドロワーナビゲーションの設定
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.navigation_view)
+        setupDrawerContent(navigationView)
 
         // Fragmentの生成
         val selectFragment: SelectFragment = SelectFragment.newInstance()
@@ -19,5 +42,42 @@ class SelectActivity : AppCompatActivity() {
 
         // Presenterの生成
         mSelectPresenter = SelectPresenter(selectFragment, applicationContext)
+    }
+
+    /**
+     * ドロワーナビゲーションのアイテムがタップされたときの処理
+     * @param navigationView
+     */
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_setting -> {
+                    // 設定画面を起動する
+                    val intent = Intent(applicationContext, SettingActivity::class.java)
+                    startActivity(intent)
+                }
+                else -> {
+                    // do nothing.
+                }
+            }
+            // ドロワーのアイテムをタップされたときは必ずドロワーを閉じる
+            menuItem.isChecked = true
+            mDrawerLayout?.closeDrawers()
+            true
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                // ハンバーガー画像をタップされたらドロワーナビゲーションを表示
+                mDrawerLayout?.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
