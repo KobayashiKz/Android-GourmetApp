3c3767af692e6583368a65f0fff5f5077157bc38
KobayashiKz <anuuihsa9711212@gmail.com>
2018-12-08 22:08:40 +0900

    ウィジェット作成
    ・タップ時に起動しない不具合修正
    ・UI調整

--- app/src/main/java/com/kk/gourmetapp/widget/WidgetProvider.kt
+++ app/src/main/java/com/kk/gourmetapp/widget/WidgetProvider.kt
@@ -5,11 +5,9 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.widget.RemoteViews
 import com.kk.gourmetapp.R
 import com.kk.gourmetapp.recommend.RecommendActivity
-
 
 class WidgetProvider : AppWidgetProvider() {
 
@@ -19,36 +17,18 @@ class WidgetProvider : AppWidgetProvider() {
 
         val views = RemoteViews(context?.packageName, R.layout.widget_layout)
 
-        val imageIntent = Intent(IMAGE_ACTION)
-        val pendingIntent: PendingIntent? = PendingIntent.getBroadcast(
-            context, WIDGET_REQUEST_CODE, imageIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
+        // タップ時にアプリを起動させるようにPendingIntentを設定
+        val intent = Intent(context, RecommendActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context,
+            WIDGET_REQUEST_CODE, intent, 0)
         views.setOnClickPendingIntent(R.id.widget_image_view, pendingIntent)
 
         // ウィジェットの更新
         appWidgetManager?.updateAppWidget(appWidgetIds, views)
     }
 
-
-
-    override fun onReceive(context: Context?, intent: Intent?) {
-        super.onReceive(context, intent)
-        Log.d("kkkk", "receive")
-
-        val action: String? = intent?.action
-
-        Log.d("kkkk", "receive: " + action)
-
-        if (action.equals(IMAGE_ACTION)) {
-            // おすすめ画面起動
-            val recommendIntent = Intent(context, RecommendActivity::class.java)
-            context?.startActivity(recommendIntent)
-        }
-    }
-
     companion object {
-        // インテントフィルタ
-        const val IMAGE_ACTION: String = "com.kk.gourmetapp.widget.IMAGE_CLICKED"
         // リクエストコード
         const val WIDGET_REQUEST_CODE: Int = 0
     }
